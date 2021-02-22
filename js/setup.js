564df7a669a6a88276229a16099967e2263c088f
Vika <queenviktoriyphp@gmail.com>
2021-02-22 21:14:32 +0200

    при нажати добавила попап и клавиатури теж

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,5 @@
 var userDialog = document.querySelector(".setup");
-userDialog.classList.remove("hidden");
+// userDialog.classList.remove("hidden");
 
 var similarListElement = document.querySelector(".setup-similar-list");
 
@@ -54,6 +54,8 @@ var wizardList = [
 
 //   return wizardElement;
 // };
+
+//заповнюємо даними з масива
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -77,10 +79,105 @@ var renderWizard = function (wizard) {
   // similarListElement.appendChild(wizardElement);
   return wizardElement;
 };
-
+//вставляємо через createDocumentFragment
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < 4; i++) {
   fragment.appendChild(renderWizard(wizardList[i]));
 }
 similarListElement.appendChild(fragment);
 userDialog.querySelector(".setup-similar").classList.remove("hidden");
+
+//взаємодія з пользовательом, при нажатти
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var setupOpen = document.querySelector(".setup-open");
+var setupСlose = userDialog.querySelector(".setup-close");
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  userDialog.classList.remove("hidden");
+  document.addEventListener("keydown", onPopupEscPress);
+};
+var closePopup = function () {
+  userDialog.classList.add("hidden");
+  document.removeEventListener("keydown", onPopupEscPress);
+};
+
+setupOpen.addEventListener("click", function () {
+  openPopup();
+});
+
+setupСlose.addEventListener("click", function () {
+  closePopup();
+});
+
+setupСlose.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+console.log(setupСlose);
+
+var setupOpenIcon = document.querySelector(".setup-open-icon");
+
+const showPopupIfClickEnter = (evt) => {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+};
+const closePopupIfClickEnter = (evt) => {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+};
+
+setupOpenIcon.addEventListener("mouseenter", function () {
+  // userDialog.classList.remove("hidden");
+  document.addEventListener("keydown", showPopupIfClickEnter);
+});
+setupOpenIcon.addEventListener("mouseleave", function () {
+  // userDialog.classList.remove("hidden");
+  document.removeEventListener("keydown", showPopupIfClickEnter);
+});
+setupСlose.addEventListener("mouseenter", function () {
+  // userDialog.classList.remove("hidden");
+  document.addEventListener("keydown", closePopupIfClickEnter);
+});
+setupСlose.addEventListener("mouseleave", function () {
+  // userDialog.classList.remove("hidden");
+  document.removeEventListener("keydown", closePopupIfClickEnter);
+});
+
+// зміна плаща
+var setupWizard = document.querySelector(".setup-wizard");
+var wizardCoat = setupWizard.querySelector(".wizard-coat");
+
+//валідація
+var userNameInput = userDialog.querySelector(".setup-user-name");
+userNameInput.addEventListener("invalid", function (evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity(
+      "Ех, чувак, закоротке імя перевір ще раз паспорт"
+    );
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity("Ого, тебе що і правда так мама назвала?!");
+  } else if (userNameInput.validity.typeMismatch) {
+    userNameInput.setCustomValidity("Дурнішого імя я ще не бачив");
+  } else {
+    userNameInput.setCustomValidity(" ");
+  }
+});
+// userNameInput.addEventListener("input", function (evt) {
+//   var target = evt.target;
+//   if (target.value.length < 2) {
+//     target.setCustomValidity("Ех, чувак, закоротке імя перевір ще раз паспорт");
+//   } else {
+//     target.setCustomValidity(" ");
+//   }
+// });
