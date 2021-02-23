2936c17b1d5f34a0c3e987743d29c4bdef3ed99c
Vika <queenviktoriyphp@gmail.com>
2021-02-23 17:11:22 +0200

    зминила при кл?ку щоб м?нялося пальто  та очи

--- js/setup.js
+++ js/setup.js
@@ -42,6 +42,9 @@ var wizardList = [
   {
     eyesColor: ["black", "red", "blue", "yellow", "green"],
   },
+  {
+    fireballColor: ["#ee4830", "#30a8ee", "#5ce6c0", "#e848d5", "#e6e848"],
+  },
 ];
 // console.log(wizardList);
 // console.log(wizardList[0].name[1]);
@@ -157,6 +160,33 @@ setupСlose.addEventListener("mouseleave", function () {
 // зміна плаща
 var setupWizard = document.querySelector(".setup-wizard");
 var wizardCoat = setupWizard.querySelector(".wizard-coat");
+var wizardEyes = setupWizard.querySelector(".wizard-eyes");
+var fireballWrap = document.querySelector(".setup-fireball-wrap");
+var inputFireColor = fireballWrap.querySelector("input");
+wizardCoat.addEventListener("click", function () {
+  wizardCoat.style.fill =
+    wizardList[1].coatColor[
+      Math.floor(Math.random() * wizardList[1].coatColor.length)
+    ];
+});
+wizardEyes.addEventListener("click", function () {
+  wizardEyes.style.fill =
+    wizardList[2].eyesColor[
+      Math.floor(Math.random() * wizardList[2].eyesColor.length)
+    ];
+});
+var changeFireColor = function () {
+  fireballWrap.addEventListener("click", function () {
+    // inputFireColor.value = fireballWrap.style.background;
+    const color =
+      wizardList[3].fireballColor[
+        Math.floor(Math.random() * wizardList[3].fireballColor.length)
+      ];
+    fireballWrap.style.background = color;
+    inputFireColor.value = color;
+  });
+};
+changeFireColor();
 
 //валідація
 var userNameInput = userDialog.querySelector(".setup-user-name");
