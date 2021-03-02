12f750b0ed622396acbfbe45df5648f0f94cd3ca
Vika <queenviktoriyphp@gmail.com>
2021-03-02 21:08:52 +0200

    задала инкапсуляцию в двох модулях

--- js/scrict.js
+++ js/scrict.js
@@ -1,19 +1,21 @@
-var fireballSize = 22;
-var getFireballSpeed = function (left) {
-  return (left = true) ? 5 : 2;
-};
+(function () {
+  window.fireballSize = 33;
+  window.getFireballSpeed = function (left) {
+    return (left = true) ? 5 : 2;
+  };
 
-var wizardSpeed = 3;
-var wizardWidth = 70;
+  window.wizardSpeed = 3;
+  var wizardWidth = 70;
 
-var getWizardHeight = function () {
-  return wizardWidth * 1.337;
-};
-function getWizardX(width) {
-  return (width - wizardWidth) / 2;
-}
-getWizardX(700);
-function getWizardY(height) {
-  return height / 3;
-}
-getWizardY(300);
+  window.getWizardHeight = function () {
+    return wizardWidth * 1.337;
+  };
+  function getWizardX(width) {
+    return (width - wizardWidth) / 2;
+  }
+  getWizardX(700);
+  function getWizardY(height) {
+    return height / 3;
+  }
+  getWizardY(300);
+})();
--- js/stat.js
+++ js/stat.js
@@ -3,69 +3,71 @@
 // var CLOUD_X = 100;
 // var CLOUD_Y = 10;
 // var bar_height = 150;
+(function () {
+  window.renderStatistics = function (ctx, names, times) {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+    ctx.fillRect(150, 20, 420, 270);
 
-window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-  ctx.fillRect(150, 20, 420, 270);
+    ctx.fillStyle = "#26C6DA";
+    ctx.fillRect(140, 10, 420, 270);
 
-  ctx.fillStyle = "#26C6DA";
-  ctx.fillRect(140, 10, 420, 270);
-
-  ctx.textBaseline = "hanging";
-  ctx.font = "16px PT Mono";
-  ctx.fillStyle = "#F5F5F5";
-
-  ctx.fillText("Ура вы победили! ", 270, 16);
-  ctx.fillText(" Список результатов:", 250, 40);
-
-  console.log(names, times);
-  // ctx.shadowColor = "red";
-  // ctx.shadowOffsetX = 10;
-  // ctx.shadowOffsetY = 10;
-
-  // ctx.textBaseline = "hanging";
-  ctx.font = "16px PT Mono";
-  ctx.fillStyle = "#000";
-
-  // ctx.fillText(names[1], 250, 240);
-  // ctx.fillText(names[2], 330, 240);
-  // ctx.fillText(names[3], 410, 240);
-
-  console.log(names[1]);
-
-  var highTime = 9000;
-  var highBar = 150;
-  // var barHeight = (times[1] * highBar) / highTime;
-  // console.log(barHeight);
-  const startMargin = 170;
-  const marginBetweenBlock = 80;
-  let colorGraph = [
-    "#7CB342",
-    "#303F9F",
-    "#D81B60",
-    "#00BCD4",
-    "#CDDC39",
-    "#4DD0E1",
-  ];
-  let color = {
-    // myColor: "rgba(255, 0, 0, 1)",
-    // myIdentity: names["Вы"],
-  };
-  for (let i = 0; i < times.length; i++) {
-    let barHeight = (times[i] * highBar) / highTime;
-    console.log(barHeight);
     ctx.textBaseline = "hanging";
     ctx.font = "16px PT Mono";
     ctx.fillStyle = "#F5F5F5";
-    ctx.fillText(names[i], startMargin + marginBetweenBlock * i, 240);
 
-    // ctx.fillStyle = color;
-    ctx.fillStyle = colorGraph[Math.floor(Math.random() * colorGraph.length)];
-    ctx.fillRect(startMargin + marginBetweenBlock * i, 225, 40, -1 * barHeight);
-  }
+    ctx.fillText("Ура вы победили! ", 270, 16);
+    ctx.fillText(" Список результатов:", 250, 40);
 
-  // ctx.fillRect(170, 155, 40, 155 - barHeight);
-  // ctx.fillRect(250, 155, 40, 155 - barHeight);
-  // ctx.fillRect(330, 155, 40, 155 - barHeight);
-  // ctx.fillRect(410, 155, 40, 155 - barHeight);
-};
+    console.log(names, times);
+    // ctx.shadowColor = "red";
+    // ctx.shadowOffsetX = 10;
+    // ctx.shadowOffsetY = 10;
+
+    // ctx.textBaseline = "hanging";
+    ctx.font = "16px PT Mono";
+    ctx.fillStyle = "#000";
+
+    // ctx.fillText(names[1], 250, 240);
+    // ctx.fillText(names[2], 330, 240);
+    // ctx.fillText(names[3], 410, 240);
+
+    console.log(names[1]);
+
+    var highTime = 9000;
+    var highBar = 150;
+    // var barHeight = (times[1] * highBar) / highTime;
+    // console.log(barHeight);
+    const startMargin = 170;
+    const marginBetweenBlock = 80;
+    let colorGraph = [
+      "#7CB342",
+      "#303F9F",
+      "#D81B60",
+      "#00BCD4",
+      "#CDDC39",
+      "#4DD0E1",
+    ];
+
+    for (let i = 0; i < times.length; i++) {
+      let barHeight = (times[i] * highBar) / highTime;
+      console.log(barHeight);
+      ctx.textBaseline = "hanging";
+      ctx.font = "16px PT Mono";
+      ctx.fillStyle = "#F5F5F5";
+      ctx.fillText(names[i], startMargin + marginBetweenBlock * i, 240);
+
+      ctx.fillStyle = colorGraph[Math.floor(Math.random() * colorGraph.length)];
+      ctx.fillRect(
+        startMargin + marginBetweenBlock * i,
+        225,
+        40,
+        -1 * barHeight
+      );
+    }
+
+    // ctx.fillRect(170, 155, 40, 155 - barHeight);
+    // ctx.fillRect(250, 155, 40, 155 - barHeight);
+    // ctx.fillRect(330, 155, 40, 155 - barHeight);
+    // ctx.fillRect(410, 155, 40, 155 - barHeight);
+  };
+})();
