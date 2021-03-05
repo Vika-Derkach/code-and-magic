edcf5858dab97b5196b06f23e2358f29082dfef2
Vika <queenviktoriyphp@gmail.com>
2021-03-05 17:11:47 +0200

    поминяла персонажией похожих

--- js/setup.js
+++ js/setup.js
@@ -51,31 +51,63 @@ var wizardList = [
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector(".setup-similar-label").textContent =
-    wizardList[0].name[Math.floor(Math.random() * wizardList[0].name.length)] +
-    " " +
-    wizardList[0].surname[
-      Math.floor(Math.random() * wizardList[0].surname.length)
-    ];
+  wizardElement.querySelector(".setup-similar-label").textContent = wizard.name;
+  // wizardList[0].name[Math.floor(Math.random() * wizardList[0].name.length)] +
+  // " " +
+  // wizardList[0].surname[
+  //   Math.floor(Math.random() * wizardList[0].surname.length)
+  // ];
 
-  wizardElement.querySelector(".wizard-coat").style.fill =
-    wizardList[1].coatColor[
-      Math.floor(Math.random() * wizardList[1].coatColor.length)
-    ];
+  wizardElement.querySelector(".wizard-coat").style.fill = wizard.colorCoat;
+  // wizardList[1].coatColor[
+  //   Math.floor(Math.random() * wizardList[1].coatColor.length)
+  // ];
 
-  wizardElement.querySelector(".wizard-eyes").style.fill =
-    wizardList[2].eyesColor[
-      Math.floor(Math.random() * wizardList[2].eyesColor.length)
-    ];
+  wizardElement.querySelector(".wizard-eyes").style.fill = wizard.colorEyes;
+  // wizardList[2].eyesColor[
+  //   Math.floor(Math.random() * wizardList[2].eyesColor.length)
+  // ];
 
   // similarListElement.appendChild(wizardElement);
   return wizardElement;
 };
 
 //вставляємо через createDocumentFragment
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < 4; i++) {
-  fragment.appendChild(renderWizard(wizardList[i]));
-}
-similarListElement.appendChild(fragment);
-userDialog.querySelector(".setup-similar").classList.remove("hidden");
+// var fragment = document.createDocumentFragment();
+// for (var i = 0; i < 4; i++) {
+//   fragment.appendChild(renderWizard(wizardList[i]));
+// }
+// similarListElement.appendChild(fragment);
+// userDialog.querySelector(".setup-similar").classList.remove("hidden");
+
+//відправляємо форму , і закриваємо вікно setup
+var form = userDialog.querySelector(".setup-wizard-form");
+form.addEventListener("submit", function (evt) {
+  window.upload(new FormData(form), function (response) {
+    userDialog.classList.add("hidden");
+  });
+  evt.preventDefault();
+});
+//получаємо магов
+var successHandler = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  for (let i = 0; i < 4; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+  userDialog.querySelector(".setup-similar").classList.remove("hidden");
+};
+var errorHandler = function (errorMessage) {
+  var node = document.createElement("div");
+  node.style =
+    "z-index: 100; margin: 0 auto; text-align: center; background-color: red;";
+  node.style.position = "absolute";
+  node.style.left = 0;
+  node.style.right = 0;
+  node.style.fontSize = "30px";
+
+  node.textContent = errorMessage;
+
+  document.body.insertAdjacentElement("afterbegin", node);
+};
+window.load(successHandler, errorHandler);
